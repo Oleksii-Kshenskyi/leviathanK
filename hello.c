bd2232ae9d08f3e1bbd544f302a045af3fff6c7f
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-04-02 00:43:52 +0300

    [0.0.7] Library + Executable setup is building:
    - Added a hello world shared library to the project;
    - Linked the library with the executable;
    - Used a function from the library in the executable;
    - TODO: create a proper folder structure to be able to start developing the project.

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "libhello.h"
 
 int main()
 {
   printf("Hello leviathanK World!\n");
+  printf("LibHello has calculated 35 + 66, the result is %d!\n", add(35, 66));
 
   return 0;
 }
---
+++ libhello.c
@@ -0,0 +1,6 @@
+#include "libhello.h"
+
+int add(int a, int b)
+{
+  return a + b;
+}
