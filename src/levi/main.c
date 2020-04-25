80c3d1c74f4b3fbca783e59c51c25692b4379373
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-04-25 11:00:43 +0300

    [0.4.3] [WIP] The first extremely rough version of shell main loop implemented:
    - Implemented a while(TRUE) loop function. It only has debugging printfs so far;
    - TODO: start parsing and executing commands with the main loop;
    - TODO: command still has to be translated from a string to a command name + void* data;
    - TODO: implement data capsules: converting from an InitialCommandData struct which has memory structs and the original command string to a proper DataCapsule for this specific command;
    - TODO: implement those commands: exit, test. Insert and print have their own issues, those are out of scope of this one;
    - TODO: remove util_string_chop_name_off_path_noalloc() and use util_string_split_step(SPLIT_KEEP_EMPTY) instead.

--- src/levi/main.c
+++ src/levi/main.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "liblevi.h"
+
+void main_levi_shell_loop(Memory* application_memory)
+{
+   while(TRUE)
+   {
+      Memory throwaway_memory = memory_create(1 * KB);
+      printf("|LEVI|> ");
+      char* command_string = memory_allocate(&throwaway_memory, 1 * KB);
+      size_t zero = 0;
+      getline(&command_string, &zero, stdin);
+      command_string[strlen(command_string) - 1] = '\0';
+      printf("[DEBUG] Your argument string is [%s]\n", command_string);
+      printf("[DEBUG] String lentgh is [%u]\n", strlen(command_string));
+      printf("[DEBUG] (strlen - 1)'th character is [%d]\n", command_string[strlen(command_string) - 1]);
+      free(throwaway_memory.pointer);
+   }
+}
 
 int main()
 {
-  printf("Hello leviathanK World!\n");
-  printf("LibHello has calculated 35 + 66, the result is %d!\n", add(35, 66));
-
-  return 0;
+   Memory application_memory = memory_create(1 * MB);
+   main_levi_shell_loop(&application_memory);
 }
