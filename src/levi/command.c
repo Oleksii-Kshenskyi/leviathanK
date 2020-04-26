8385f907a4413ead4a4999304987e5b7f32e10f2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-04-26 16:13:20 +0300

    [0.4.6] Fixed a segmentation fault in the test command:
    - It turned out the segmentation fault was because test_data_capsule was a part of throwaway_memory itself, and when trying to free memory I was addressing it via test_data_capsule that had already been freed before that point. Worked around that with an additional pointer variable;
    - TODO: implement exit command;
    - TODO: implement "Available Commands" at the greeting prompt;
    - TODO: remove util_string_chop_name_off_path_noalloc() and use util_string_split_step(SPLIT_KEEP_EMPTY) instead.

--- src/levi/command.c
+++ src/levi/command.c
@@ -4,6 +4,9 @@
 #include "command.h"
 #include "util.h"
 
+#define TEST_NO_ARGUMENT_RESPONSE "[TEST] No argument provided.\n"
+#define TEST_RESPONSE_WITH_ARGUMENT "[TEST] Argument provided: [%s]\n"
+
 struct TestDataCapsule
 {
    char* test_argument;
@@ -16,19 +19,19 @@ void* command_test_create_data_capsule(struct ShellCommand* command, struct Init
    assert(command);
    assert(initial_data);
 
-   size_t arg_len = strlen(initial_data->command_string);
-   size_t output_size = 20 + arg_len;
+   unsigned short is_command_string_empty = !strcmp(initial_data->command_string, "");
+   size_t output_size = strlen(initial_data->command_string) + strlen(is_command_string_empty ? TEST_NO_ARGUMENT_RESPONSE : TEST_RESPONSE_WITH_ARGUMENT);
+   size_t memory_size = output_size * 2 + sizeof(struct TestDataCapsule);
 
-   struct Memory* throwaway_memory = memory_create_heap(output_size + sizeof(struct TestDataCapsule) + 120);
+   struct Memory* throwaway_memory = memory_create_heap(memory_size);
    struct TestDataCapsule* capsule = memory_allocate(throwaway_memory, sizeof(struct TestDataCapsule));
 
-   if(!strcmp(initial_data->command_string, ""))
+   if(is_command_string_empty)
       capsule->test_argument = NULL;
    else 
       capsule->test_argument = util_string_create(throwaway_memory, initial_data->command_string, 0);
    capsule->throwaway_memory = throwaway_memory;
-   capsule->command_output = memory_allocate(throwaway_memory, output_size);
-   strcpy(capsule->command_output, "");
+   capsule->command_output = util_string_create(throwaway_memory, "", output_size);
 
    return capsule;
 }
@@ -37,9 +40,9 @@ void* command_test_execute(void* data_capsule)
 {
    struct TestDataCapsule* test_data_capsule = data_capsule;
    if(test_data_capsule->test_argument)
-      sprintf(test_data_capsule->command_output, "[TEST] Argument provided: [%s]\n", test_data_capsule->test_argument);
+      sprintf(test_data_capsule->command_output, TEST_RESPONSE_WITH_ARGUMENT, test_data_capsule->test_argument);
    else
-      sprintf(test_data_capsule->command_output, "[TEST] No argument provided.\n");
+      sprintf(test_data_capsule->command_output, TEST_NO_ARGUMENT_RESPONSE);
    return data_capsule;
 }
 
@@ -49,6 +52,7 @@ void command_test_process_result(void* data_capsule)
 
    printf(test_data_capsule->command_output);
 
-   free(test_data_capsule->throwaway_memory->pointer);
-   free(test_data_capsule->throwaway_memory);
+   struct Memory* memory = test_data_capsule->throwaway_memory;
+   free(memory->pointer);
+   free(memory);
 }
